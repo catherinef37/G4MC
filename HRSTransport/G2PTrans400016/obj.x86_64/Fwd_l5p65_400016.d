7beef0f64d3f63caf75b53b53ed9fba627fb9481
Seamus Riordan <seamus@riordan.io>
2014-12-17 07:22:47 -0500

    Pull out a lot of backup files and system-dependent build files

--- HRSTransport/G2PTrans400016/obj.x86_64/Fwd_l5p65_400016.d
+++
@@ -1,1 +0,0 @@
-obj.x86_64/Fwd_l5p65_400016.o obj.x86_64/Fwd_l5p65_400016.d: Fwd_l5p65_400016.cpp Fwd_l5p65_400016.h
--- HRSTransport/hamcPREXTrans/obj.x86_64/hamcPREXTrans.d
+++
@@ -1,2 +0,0 @@
-obj.x86_64/hamcPREXTrans.o obj.x86_64/hamcPREXTrans.d: hamcPREXTrans.cc prex_forward.h hamcPREXTrans.hh \
- ../HRSTransBase.hh
